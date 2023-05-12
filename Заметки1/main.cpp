7199a0773a5e2bed8b3ec0dc9706cfde377df7fa
User8 <example@gmail.com>
2023-05-12 20:04:42 +0300

    Доработка

--- Заметки1/main.cpp
+++ Заметки1/main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "");
     srand(time(0) * time(0));
-    cout << "Текстовый квест";
+    cout << "Текстовый квест" << endl;
+    cout << "Данный квест подразумевает введение одного правильного ответа состоящего из целых цифр без пробелов";
     cout << endl << endl;
 
     int num = 1;
@@ -15,43 +16,81 @@ int main() {
         cout << "1. Да" << endl;
         cout << "2. Нет" << endl;
         cin >> num;
-        switch (num) {
-        case 1: {
-            int a;
-            cout << "Ответьте на вопрос" << endl;
-            cout << "Сколько дней в феврале?" << endl << endl;
-            cin >> a;
-            if (a == 28) {
-                cout << "Ответ правильный!" << endl << endl;
+        if (num == 1) {
+            while (true) {
+                int a;
+                cout << "Ответьте на вопрос" << endl;
+                cout << "Сколько дней в феврале?" << endl << endl;
+                cin >> a;
+                if (a == 28) {
+                    cout << "Ответ правильный!" << endl << endl;
+                    break;
+                }
+                else {
+                    cout << "Ответ не правильный" << endl << endl;
+                    continue;
+                }
             }
-            else {
-                cout << "Ответ не правильный" << endl << endl;
+            while (true) {
+                int b;
+                cout << "Ответьте на вопрос" << endl << endl;
+                cout << "Какой сейчас год?" << endl << endl;
+                cin >> b;
+                if (b == 2023) {
+                    cout << "Ответ правильный!" << endl << endl;
+                    break;
+                }
+                else {
+                    cout << "Ответ не правильный" << endl << endl;
+                    continue;
+                }
             }
-            int b;
-            cout << "Ответьте на вопрос" << endl << endl;
-            cout << "Какой сейчас год?" << endl << endl;
-            cin >> b;
-            if (b == 2023) {
-                cout << "Ответ правильный!" << endl << endl;
+            while (true) {
+                int c;
+                cout << "Ответьте на вопрос" << endl << endl;
+                cout << "В каком году было отменено крепостное право?" << endl << endl;
+                cin >> c;
+                if (c == 1861) {
+                    cout << "Ответ правильный!" << endl << endl;
+                    break;
+                }
+                else {
+                    cout << "Ответ не правильный" << endl << endl;
+                    continue;
+                }
             }
-            else {
-                cout << "Ответ не правильный" << endl << endl;
+            while (true) {
+                int c;
+                cout << "Ответьте на вопрос" << endl << endl;
+                cout << "Сколько месяцев в году?" << endl << endl;
+                cin >> c;
+                if (c == 12) {
+                    cout << "Ответ правильный!" << endl << endl;
+                    break;
+                }
+                else {
+                    cout << "Ответ не правильный" << endl << endl;
+                    continue;
+                }
             }
-            int c;
-            cout << "Ответьте на вопрос" << endl << endl;
-            cout << "В каком году было отменено крепостное право?" << endl << endl;
-            cin >> c;
-            if (c == 1861) {
-                cout << "Ответ правильный!" << endl << endl;
-            }
-            else {
-                cout << "Ответ не правильный" << endl << endl;
+            while (true) {
+                int c;
+                cout << "Ответьте на вопрос" << endl << endl;
+                cout << "Сколько букв в русском алфавите?" << endl << endl;
+                cin >> c;
+                if (c == 33) {
+                    cout << "Ответ правильный!" << endl << endl;
+                    break;
+                }
+                else {
+                    cout << "Ответ не правильный" << endl << endl;
+                    continue;
+                }
             }
         }
-
-
-        }
-        
+        else {
+            cout << "Конец игры";
+        } 
     }
     return 0;
 }
