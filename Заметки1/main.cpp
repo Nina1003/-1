9d0edf8439b5026bf9e6dbf4110558e246c63e07
User8 <example@gmail.com>
2023-05-12 20:57:39 +0300

    Экзамен доработка

--- Заметки1/main.cpp
+++ Заметки1/main.cpp
@@ -6,9 +6,12 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "");
     srand(time(0) * time(0));
-    cout << "Текстовый квест" << endl;
-    cout << "Данный квест состоит из пяти вопросов и подразумевает введение одного правильного ответа состоящего из целых цифр без пробелов" << endl;
+    cout << "Текстовый квест" << endl << endl;
+    cout << "Данный квест состоит из пяти вопросов" << endl;
+    cout << "Он подразумевает введение одного правильного ответа состоящего из целых цифр без пробелов" << endl;
     cout << "В случае введения неверного ответа, вопрос будет повторен до тех пор, пока ответ не будет верен" << endl;
+    cout << "За каждый правильный ответ Вам будет начисляться 5 баллов. Когда вы заработаете 25 баллов - Вы выиграете!" << endl;
+    cout << "Удачи Вам!" << endl;
     cout << endl << endl;
 
     int num = 1;
@@ -17,6 +20,7 @@ int main() {
         cout << "1. Да" << endl;
         cout << "2. Нет" << endl;
         cin >> num;
+        int points = 0;
         if (num == 1) {
             while (true) {
                 int a;
@@ -25,6 +29,8 @@ int main() {
                 cin >> a;
                 if (a == 28) {
                     cout << "Ответ правильный!" << endl << endl;
+                    points += 5;
+                    cout << "У вас " << points << " баллов" << endl << endl;
                     break;
                 }
                 else {
@@ -39,6 +45,8 @@ int main() {
                 cin >> b;
                 if (b == 2023) {
                     cout << "Ответ правильный!" << endl << endl;
+                    points += 5;
+                    cout << "У вас " << points << " баллов" << endl << endl;
                     break;
                 }
                 else {
@@ -53,6 +61,8 @@ int main() {
                 cin >> c;
                 if (c == 1861) {
                     cout << "Ответ правильный!" << endl << endl;
+                    points += 5;
+                    cout << "У вас " << points << " баллов" << endl << endl;
                     break;
                 }
                 else {
@@ -67,6 +77,8 @@ int main() {
                 cin >> c;
                 if (c == 12) {
                     cout << "Ответ правильный!" << endl << endl;
+                    points += 5;
+                    cout << "У вас " << points << " баллов" << endl << endl;
                     break;
                 }
                 else {
@@ -81,6 +93,9 @@ int main() {
                 cin >> c;
                 if (c == 33) {
                     cout << "Ответ правильный!" << endl << endl;
+                    points += 5;
+                    cout << "У вас " << points << " баллов" << endl << endl;
+                    cout << "ПОБЕДА!" << endl << endl;
                     break;
                 }
                 else {
